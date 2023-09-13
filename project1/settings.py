4f1aba893bd8dd4d549ce7f4a8a6a48a598e9db1
Ruban14 <rubanbharath14@gmail.com>
2023-09-13 17:52:22 +0530

    updated static root

--- project1/settings.py
+++ project1/settings.py
@@ -123,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
